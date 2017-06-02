1df16aafd55143b7202432e95d042caca65e0060
Arc <1034944@qq.com>
2017-06-02 13:57:33 +0800

    new notebook

--- zhihu.py
+++ zhihu.py
@@ -6,13 +6,13 @@ import urllib
 import urllib2
 
 headers = {
-
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
                }
 
 
 def get_answer_list(maxpage):
     answerlist = []
-    for page in range(1, maxpage+1):
+    for page in range(17, maxpage+1):
         url = 'https://www.zhihu.com/collection/78172986?page=%s' % page
         request = urllib2.Request(url, headers=headers)
         response = urllib2.urlopen(request)
@@ -42,11 +42,19 @@ def get_img_list(maxpage):
     return imglist
 
 
-def cbk(a, b, c):
-    per = 100.0 * a * b / c
-    if per > 0:
+def cbk(block_read, block_size, total_size):
+    amout_size = block_read * block_size
+    per = 100 * amout_size / float(total_size)
+    if per > 100:
         per = 100
-    print '%.2f%%' % per
+    if not block_read:
+        print "connection opened"
+    elif total_size < 0:
+        # unknown size
+        print "Downloaded %d blocks (%dbytes)" % (block_read, amout_size)
+    else:
+        print 'Downloaded %.2f%% ------------ %d blocks,or %d/%d ' % (per, block_read, amout_size, total_size)
+        # print 'Read %d blocks,or %d/%d' % (block_read, block_read*block_size, total_size)
 
 
 def down_img(maxpage):
@@ -56,5 +64,8 @@ def down_img(maxpage):
         local = 'C:\\Users\\Arc\\PycharmProjects\\urllib-study\\img\\' + imgname
         urllib.urlretrieve(img, local, cbk)
     print 'All images have been downloaded.'
+    print 'The last downloaded answer is %s' % abs_answer_list(maxpage)[-1]
+    with open('last_answer.txt', 'a') as f:
+        f.write('Last page:%s' % maxpage + ' Last answer:%s' % abs_answer_list(maxpage)[-1]+'\n')
 
-down_img(1)
+down_img(17)
